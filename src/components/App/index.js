9b116f21a6965d0c688eec6d5084d7769f415a03
kanitsharma <kanitshrm09@gmail.com>
2018-04-24 20:00:30 +0530

    Added working example

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./index.css";
+
+const App = props => (
+  <div className={styles.App}>
+    Hello React!
+  </div>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello')
+import React from "react"
+import ReactDOM from "react-dom"
+import App from './components/App'
+import './index.css'
+
+const Main = () => {
+  return <App />
+}
+
+ReactDOM.render(<Main />, document.getElementById("index"))
